c37999fc0d71cbc6da4c7c767171c5dbd8fbd3cb
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-08-31 13:18:15 -0400

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/paper_towels", controller: "products", action: "paper_towels"
+
+  get "/clorox_bleach", controller: "products", action: "clorox_bleach"
+
+  get "/dyson_v10_vacuum", controller: "products", action: "dyson_vacuum"
+
+  get "/dawn_soap", controller: "products", action: "dawn_soap"
+
+  get "tide_pods", controller: "products", action: "tide_pods"
 end
