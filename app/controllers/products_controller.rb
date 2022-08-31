3a5a34aeb9f2d3d2686177861e798927ecf8fd53
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-08-31 13:22:30 -0400

    added methods to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,26 @@
 class ProductsController < ApplicationController
+  def paper_towels
+    product = Product.first
+    render json: product
+  end
+
+  def clorox_bleach
+    product = Product.second
+    render json: product
+  end
+
+  def dyson_vacuum
+    product = Product.third
+    render json: product
+  end
+
+  def dawn_soap
+    product = Product.fourth
+    render json: product
+  end
+
+  def tide_pods
+    product = Product.fifth
+    render json: product
+  end
 end
