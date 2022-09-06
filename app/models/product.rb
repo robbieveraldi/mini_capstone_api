200ac5d9d1c6c996076ab9b2f61b26f0cf120c55
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-09-06 12:54:48 -0400

    implemented happy/sad paths to return message when data validation fails

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :description, length: { in: 10..500 }
-  validates :image_url, with: %r{\.(png|jpg|jpeg)$}i
+  validates_format_of :image_url, with: %r{\.(png|jpg|jpeg)$}i, message: "Must have valid file type", multiline: true
 
   def is_discounted?
     price.to_i < 10
