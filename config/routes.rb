ddb418e147fa3dd93e87a1251f0e4572694ef4ac
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-09-07 12:27:20 -0400

    created migration to remove image_url from products table

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,16 @@ Rails.application.routes.draw do
   post "/products" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+
+  #supplier routes
+  get "/suppliers/:id" => "suppliers#show"
+  get "/suppliers" => "suppliers#index"
+  post "/suppliers" => "suppliers#create"
+  patch "/suppliers/:id" => "suppliers#update"
+  delete "/suppliers/:id" => "suppliers#destroy"
 end
 
 #supplier routes
-get "/suppliers/:id" => "suppliers#show"
-get "/suppliers" => "suppliers#index"
-post "/suppliers" => "suppliers#create"
-patch "/suppliers/:id" => "suppliers#update"
-delete "/suppliers/:id" => "suppliers#destroy"
 
 #alternate way to write this:
 #get "/all_products" => "products#all_products"
