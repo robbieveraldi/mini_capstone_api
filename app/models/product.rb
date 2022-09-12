c86c4eab5a917983939f91fff4a84597ce0c26e0
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-09-12 13:28:59 -0400

    added has_many through methods to category and product models. allows for category.products // or product.categories within rails console

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,7 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
 
   def is_discounted?
     price.to_i < 10
--- db/migrate/20220912163722_create_category_products.rb
+++ db/migrate/20220912163722_create_category_products.rb
@@ -1,7 +1,4 @@
 class CreateCategoryProducts < ActiveRecord::Migration[7.0]
-  belongs_to :Category
-  belongs_to :Product
-
   def change
     create_table :category_products do |t|
       t.integer :product_id
