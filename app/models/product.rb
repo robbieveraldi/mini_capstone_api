c403f759ab09806df35afbc09ea64a71eec5b388
Robbie Veraldi <robbie.veraldi@gmail.com>
2022-09-13 13:48:08 -0400

    added model associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
 
   def is_discounted?
     price.to_i < 10
